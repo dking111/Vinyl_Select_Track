3ca63625488fd284514a161f1f5aa35420e008d0
dking111 <102740816+dking111@users.noreply.github.com>
2025-06-11 21:41:50 +0100

    Calling main on button

--- IO.py
+++ IO.py
@@ -1,10 +1,15 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO 
+import os
+
+def button_callback(channel):
+    os.system('python main.py')
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback,bouncetime=1000) 
+message = input("Press enter to quit\n\n") 
+GPIO.cleanup() #
 
-while True:
-        if GPIO.input(10) == GPIO.HIGH:
-            print("pushed")
+button_callback(1)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
-    vert_line = np.array(vert_line, dtype=np.float32)
-    gradient = np.abs(np.diff(vert_line))
-    peaks, _ = find_peaks(gradient, prominence=5)
-
-    # Filter: keep only peaks where the intensity is <= 100
-    refined_peaks = [p for p in peaks if vert_line[p + 1] <= 100]
+GPIO.setwarnings(False) 
+# GPIO.setmode(GPIO.BOARD) 
+# GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+# GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) 
+# message = input("Press enter to quit\n\n") 
+# GPIO.cleanup() #
